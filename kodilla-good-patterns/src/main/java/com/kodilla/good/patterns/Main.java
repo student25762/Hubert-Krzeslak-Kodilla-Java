f9d61dc3e94639156198494b65948ca27cdfae95
student25762 <hubertkrz@gmail.com>
2019-12-19 23:14:24 +0100

    Task 13.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Main.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Main.java
@@ -4,13 +4,20 @@ import com.kodilla.good.patterns.challenges.MovieStore;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args){
         MovieStore movieStore = new MovieStore();
         Map<String, List<String>> movies = movieStore.getMovies();
-        List<String> result = movies.values().stream()
-                .flatMap(s -> s.stream().
+        List<String> result = movies.entrySet().stream()
+                .flatMap(e -> e.getValue().stream())
+                .collect(Collectors.toList());
 
+        String moviesList = result.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining("! "));
+
+        System.out.println(moviesList + "!");
     }
 }
